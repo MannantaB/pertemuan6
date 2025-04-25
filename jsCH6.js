6e1c2e1fbf3482d178783d85c7541d1a0cffea39
MannantaB <mannanta18@gmail.com>
2025-04-25 14:06:21 +0700

    first commit

---
+++ jsCH6.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video Cover Lagu Bermuara");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio Nana");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Na Jaemin");
+});
